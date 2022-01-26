2cf46b41fc34d4b09b6b7455c31320fc81357412
Maciej Koscielak <maciej.koscielak@gmail.com>
2022-01-26 13:01:16 +0100

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Maciej");
+        System.out.println(31);
+        System.out.println("K");
+
+    }
+}
