5911cb8893c0043eb58f23f25d824f0f20138240
Maciej Koscielak <maciej.koscielak@gmail.com>
2022-01-27 10:32:19 +0100

    Initial Version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String countryName = "Ireland";
+        int area = 70273;
+        double populationDensity = 63.5;
+        char symbol = 'I';
+        boolean seaAccess = true;
+        String currency = "Euro";
+
+        System.out.println(countryName);
+        System.out.println(area);
+        System.out.println(populationDensity);
+        System.out.println(symbol);
+        System.out.println(seaAccess);
+        System.out.println(currency);
+
+    }
+}
