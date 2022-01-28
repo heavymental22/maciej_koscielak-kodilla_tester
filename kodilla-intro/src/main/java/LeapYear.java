c512a3e277cd0294556d9be06391715d1ae8afe5
Maciej Koscielak <maciej.koscielak@gmail.com>
2022-01-28 08:54:04 +0100

    Initial version

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -3,15 +3,15 @@ public class LeapYear {
 
         int inputYear = 1900; //input a year
 
-        if (inputYear % 400 == 0) //if is divided by 100
+        if (inputYear % 400 == 0) //if is divided by 400
         {
             System.out.println ("PRZESTEPNY");
         }
-        else if (inputYear % 4 == 0 && inputYear % 100 != 0) //if is divided by 4 and not divided by 100
+        else if (inputYear % 4 == 0 && inputYear % 100 != 0) //if not divided by 400, but divided by 4 and not divisible by 100
         {
             System.out.println ("PRZESTEPNY");
         }
-        else
+        else // every other year
         {
             System.out.println("NIEPRZESTEPNY");
         }
